283a3d083fec9b07db1e37ac30926d9d8742fd84
adpio <adampiotrowski@Adams-MacBook-Air-2.local>
2017-10-31 11:26:38 +0100

    init

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+  'name':'Innovation Collider Corporate theme',
+  'description': 'An adaptation of Particles theme to odoo platform',
+  'version':'0.1',
+  'author':'Adam Piotrowski',
+
+  'data': [ 'views/layout.xml', 'views/assets.xml'],
+  'category': 'Theme/Creative',
+  'depends': ['website','website_blog'],
+}
