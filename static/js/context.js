54733c4c760f814f76d9d067c4a2eace3ab16eb0
adpio <adampiotrowski@Adams-MacBook-Air-2.local>
2017-10-31 15:30:47 +0100

    1

---
+++ static/js/context.js
@@ -0,0 +1,256 @@
+var context =	
+	{
+		"blog-post" : [
+			{
+				"date" : "September 2017",
+				"title" : "AI is coming",
+				"synopsis" : "Recently artificial intelligence (AI) has been the focus of a lot of speculation, anticipation, and excitement for businesses. A similar trend regarding big data occurred several years ago, however in 2015 big data was dropped from the Gartner Hype Cycle for Emerging Technology…",
+				"img-link" : "images/blog/3611.jpg",
+				"link" : "/blog/our-innovation-blog-1/post/ai-is-coming-8"
+			},
+			{
+				"date" : "April 2017",
+				"title" : "Technology review",
+				"synopsis" : "After attending 2017 4YFN and speaking with more than 400 startups we would like to report some interesting trends in services and solutions available today for banks, insurance companies and telecoms…",
+				"img-link" : "images/blog/1284.jpg",
+				"link" : "/blog/our-innovation-blog-1/post/april-2017-technology-review-2"
+			},
+			{
+				"date" : "March 2017",
+				"title" : "3 Common Problems with Retargeting Customers",
+				"synopsis" : "It is an exciting time to be involved in digital sales and marketing; there are new tools constantly being released , and new methodologies to trial. While the right strategy and necessary toolset can vary depending on each company’s goals and situation…",
+				"img-link" : "images/blog/blog_post_cover_2.jpg",
+				"link" : "/blog/our-innovation-blog-1/post/three-common-problems-1"
+			}
+		],
+		"product":[
+			{
+				"name" : "Vintom",
+				"id" : "Vintom",
+				"filters" : "awareness consideration conversion loyalty",
+				"img" : "images/partners/vintom.png",
+				"tagline" : "Save on Subscriber costs",
+				"h4" : "Automated Video Marketing",
+				"description": "<h5 class='heading-1 modal-heading mb20'>Client Engagement & Video Personalization Services (Vintom)</h5><p>Video personalization has enabled large improvements over traditional digital campaigns, achieving 300% or higher conversion rates for many organizations, along with an increase in branding through higher viewing and completion rates. </p><p>They have begun offering fully personalized video experiences embedded in websites, or available as large digital advertisements, and continue to outperform the competition with a superior understanding of how to leverage personalization towards actual sales conversions. </p><p>This product is exceptionally useful for explaining complex products or services while leveraging the availability of personalized data.</p>",
+				"infographic-link": "assets/vintom.pdf",
+				"reveal-link":"revealjs/vintom.html",
+				"infographic-img":"assets/vintom.jpg"
+			},
+			{
+				"name" : "Deep.Bi",
+				"id" : "DeepBi",
+				"filters" : "conversion loyalty consideration",
+				"img" : "images/partners/deepbi.png",
+				"tagline" : "Learn from big data",
+				"h4" : "More clarity into making decissions",
+				"description": "<h5 class='heading-1 modal-heading mb20'>Big data and deep learning services (Deep.BI)</h5><p>Digital transformation is a critical step for large incumbents in order to take advantage of the most recent waves of technical innovation, big data and artificial intelligence. Incumbents in our target industries, with a few exceptions, are not digital natives; that is to say, they were not developed from the ground-up as a digital company. </p><p>To out compete the new challengers, they need to develop a robust digital strategy, and Deep.bi does just that. Deep.bi offers benefits to companies undergoing any stage of the long process towards developing business relevant artificial intelligence. Deep.bi offers more detailed data collection, analytics, modeling, and deep machine learning to help progress companies towards better understanding their data, and better implementing the value of big data throughout their organization.</p><p>Concrete examples include implementing algorithms and predictive models to perform lead scoring, marketing channel optimization, programmatic product recommendation, and enhanced risk assessment.</p>",
+				"infographic-img" : "assets/deepbi.jpg",
+				"infographic-link" : "assets/deepbi.pdf"
+			},
+			{
+				"name" : "G1ANT",
+				"id" : "G1ANT",
+				"filters" : "conversion operations",
+				"img" : "images/partners/g1ant.jpg",
+				"tagline" : "Employ robots",
+				"h4" : "Operational improvements through RPA",
+				"description": "<h5 class='heading-1 modal-heading mb20'>Robotic Process Automation (G1ANT)</h5><p>As business is conducted in an increasingly digital manner, most roles and business functions utilize a range of software and IT systems. Robotic process automation (RPA) provides organizations with the ability to automate processes across  any computer systems, including legacy systems and custom programs, by mimicking human interaction with the software. This is of particular relevance for organizations that either have a large amount of repeatable processes, rely on many legacy systems, or struggle to integrate a wide range of systems as the result of mergers and acquisitions.</p><p>Furthermore, RPA solutions allow for processes to be completed faster, cheaper and to eliminates human error. Organizations have successfully implemented RPA for an increase in throughput, capacity, and the quality of customer service.</p><p>Examples of use include assisting call center agents perform queries involving multiple databases in order to serve the needs of their clients, or fully automating backend processes needed to open new accounts. The wide applications of use, and cost savings approaching a 10x improvement over a human operator, make RPA particularly appealing to many organizations. </p>",
+				"infographic-img" : "assets/g1ant.jpg",
+				"infographic-link" : "assets/g1ant.pdf"
+			},
+			{
+				"name" : "LeanPlum",
+				"id" : "LeanPlum",
+				"filters" : "conversion loyalty",
+				"img" : "images/partners/leanplum.jpeg",
+				"tagline" : "Connect with Customers",
+				"h4" : "A complete solution for communication",
+				"description": "<h5 class='heading-1 modal-heading mb20'>Programmatic marketing communication (LeanPlum)</h5><p>LeanPlum offers marketers everything they need to optimize digital communication, with a focus on the mobile experience. LeanPlum enables marketers to set specific KPI driven goals, and then execute on them entirely within one platform. LeanPlum is comprised of multi-variant testing, programmatic marketing campaigns, and a robust analytics platform, all while allowing dynamic refinement of the look and feel for mobile applications. They have demonstrated improving early user monetization, lowering onboarding dropoff, and improving app store ratings for their clients, among many other benefits. Their platform allows for easy, personalized, cross-channel marketing campaigns ultimately improving service delivery for the customers of their clients, and revenues for their clients.</p>",
+				"infographic-img" : "assets/leanplum.jpg",
+				"infographic-link" : "assets/leanplum.pdf"
+			},
+			{
+				"name" : "Live Call",
+				"id" : "LiveCall",
+				"filters" : "conversion loyalty",
+				"img" : "images/partners/livecall.png",
+				"tagline" : "Engage Visitors",
+				"h4" : "Engage Website Visitors",
+				"description": "",
+				"infographic-link":"assets/BrandBassadors-Click-to-Call.pdf",
+				"infographic-img": "assets/livecall.jpg",
+				"reveal-link":"revealjs/livecall.html"
+			},
+			{
+				"name" : "Sotrender",
+				"id" : "Sotrender",
+				"filters" : "awareness loyalty",
+				"img" : "images/partners/sotrender.png",
+				"tagline" : "Discover your brand",
+				"h4" : "Learn From your Competitors",
+				"description": "",
+				"infographic-link":"assets/BrandBassadors-SocialMediaAnalytics-Agency.pdf",
+				"infographic-img": "assets/sotrender.jpg",
+				"reveal-link":"revealjs/sotr.html"
+			}
+		],
+		"director" : [
+			{
+				"title":"director",
+				"id":"MarcinLesczylowski",
+				"name": "Marcin Leszczyłowski",
+				"location":"Warsaw • Poland",
+				"description": "",
+				"img":"images/colliders/marcin_leszczylowski.jpg"
+			},
+			{
+				"title":"director",
+				"id":"CarlosPerezGonzales",
+				"name": "Carlos Perez Gonzalez",
+				"location":"Barcelona • Spain",
+				"description": "",
+				"img":"images/colliders/carlos_perez_gonzalez.jpg"
+			},
+			{
+				"title":"director",
+				"id":"FernandoZerboni",
+				"name": "Prof. Fernando Zerboni",
+				"location":"Buenos Aires • Argentina",
+				"description": "",
+				"img":"images/colliders/fernando_zerboni.jpg"
+			}
+		],
+		"advisor" : [
+			{
+				"title":"advisor",
+				"id":"Gabriel-Ruiz-Tagle",
+				"name": "Gabriel Ruiz-Tagle",
+				"location":"Santiago • Chile",
+				"description": "",
+				"img":"images/colliders/gabriel_ruiz_tagle.jpg"
+			},
+			{
+				"title":"advisor",
+				"id":"DanielReinoso",
+				"name": "Daniel Reinoso",
+				"location":"Lima • Peru",
+				"description": "",
+				"img":"images/colliders/daniel_reinoso.jpg"
+			},			
+			{
+				"title":"advisor",
+				"id":"JuanPabloCaminati",
+				"name": "Juan Pablo Caminati",
+				"location":"Lima • Peru",
+				"description": "",
+				"img":"images/colliders/juan_pablo_caminati.jpg"
+			},
+			{
+				"title":"advisor",
+				"id":"DanielaMendoza",
+				"name": "Daniela Mendoza",
+				"location":"New York • USA",
+				"description": "",
+				"img":"images/colliders/daniela_mendoza.jpg"
+			},
+			{
+				"title":"advisor",
+				"id":"TomaszMorawski",
+				"name": "Tomasz Morawski",
+				"location":"Bogota • Colombia",
+				"description": "",
+				"img":"images/logo_light.png"
+			},
+			{
+				"title":"advisor",
+				"id":"LuisFelipeHerman",
+				"name": "Luis Felipe Herman",
+				"location":"Santiago • Chile",
+				"description": "",
+				"img":"images/colliders/luis_felipe_herman.jpg"
+			},
+			{
+				"title":"advisor",
+				"id":"JuliaDedo",
+				"name": "Julia Dedo",
+				"location":"Paris • France",
+				"description": "",
+				"img":"images/colliders/julia_dedo.jpg"
+			}
+		],
+		"team_member" : [
+			{
+				"id" : "AdamPiotrowski",
+				"name" : "Adam Piotrowski",
+				"description" : "Adam has a background in consulting with PwC and KPMG, and private equity in the telecom industry.",
+				"location" : "Bratislava • Slovakia",
+				"img" : "images/Adam.png"
+			},
+			{
+				"id" : "ErikLiepmann",
+				"name" : "Erik Liepmann",
+				"description" : "Having moved internationally his entire life, Erik is addicted to new opportunities, and has a	passion for implementing sharing discoveries. Reflecting this passion he has accumulated experience from the hospitality, recording arts, and pharmaceutical industries across a variety of functions.",
+				"location" : "Barcelona • Spain",
+				"img" : "images/Erik.jpg"
+			},
+			{
+				"id" : "JerzyCiecierski",
+				"name" : "Jerzy Ciecierski",
+				"description" : "Jerzy has a passion for bringing development to growing markets, having previously worked in the Polish, English, and Emirati banking sectors with HSBC. His current priority is to continue to developing economies by creating the right local opportunities.",
+				"location" : "Warsaw • Poland",
+				"img" : "images/Jurek.jpg"
+			}
+		],
+		"place":[
+			{
+				"name": "Barcelona • Spain",
+				"lat" : "41.3900",
+				"lon" : "2.1540"
+			},
+			{
+				"name": "Warsaw • Poland",
+				"lat" : "52.2296",
+				"lon" : "21.0122"
+			},
+			{
+				"name": "Santiago • Chile",
+				"lat" : "-33.4378",
+				"lon" : "-70.6504"
+			},
+			{
+				"name": "Bratislava • Slovakia",
+				"lat" : "48.1486",
+				"lon" : "17.1077"
+			},
+			{
+				"name": "Lima • Peru",
+				"lat" : "-12.2721",
+				"lon" : "-76.2711"
+			},
+			{
+				"name": "New York • USA",
+				"lat" : "40.7128",
+				"lon" : "-74.0059"
+			},
+			{
+				"name": "Paris • France",
+				"lat" : "48.8566",
+				"lon" : "2.3522"
+			},
+			{
+				"name": "Bogota • Colombia",
+				"lat" : "4.7110",
+				"lon" : "-74.0721"
+			},
+			{
+				"name": "Quito • Ecuador",
+				"lat" : "-0.1807",
+				"lon" : "-78.4678"
+			},
+			{
+				"name" : "Buenos Aires • Argentina",
+				"lat" : "-34.6037",
+				"lon" : "-58.3816"
+			}
+		]
+}
